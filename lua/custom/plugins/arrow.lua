376ce0380923fe171eb384b122a70f582886a4bd
Cruz Ogden <tcogden23@gmail.com>
2024-06-05 15:09:36 -0600

    separate arrow by git branch

--- lua/custom/plugins/arrow.lua
+++ lua/custom/plugins/arrow.lua
@@ -4,5 +4,6 @@ return {
     show_icons = true,
     leader_key = '<leader>;', -- Recommended to be a single key
     buffer_leader_key = 'm', -- Per Buffer Mappings
+    separate_by_branch = true,
   },
 }
